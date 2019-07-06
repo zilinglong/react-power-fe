51a3f5505cd5d85c017d96cf491a2738ea5e4cbd
mengmingxiu <mengmingxiu@100tal.com>
2019-07-06 18:24:24 +0800

    添加测试SCSS文件

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,8 +3,9 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="app wrap text-align padding20">
-      test
+    <div className="app wrap padding20">
+      <p className="title">这里是题目显示区域</p>
+      <p className="content">这里是内容显示区域</p>
     </div>
   );
 }
